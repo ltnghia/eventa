73360d60ee8b1f216e040b92ae85260a9adad341
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-03-28 14:09:55 +0700

    update link

--- src/app/eventa-2025/track1/components/instructions.jsx
+++ src/app/eventa-2025/track1/components/instructions.jsx
@@ -6,7 +6,7 @@ const Instruction = () => {
             </p>
             <br/>
             <p>
-                Participants also require to submit a detailed paper through <span className="font-bold"><a href='https://cmt3.research.microsoft.com/EVENTA25' className="underline text-[1.1em] text-[#232ba1]">the official challenge platform</a></span> to validate their solutions.
+                Participants also require to submit a detailed paper through <span className="font-bold"><a href='https://cmt3.research.microsoft.com/EVENTA2025' className="underline text-[1.1em] text-[#232ba1]">the official challenge platform</a></span> to validate their solutions.
             </p>
             <br/>
             <p>More details will be released soon.</p>
