e6069b7f1c824f40f30230c1524055ff4ba8ec54
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-04-14 00:16:49 +0700

    update link

--- src/app/components/news.jsx
+++ src/app/components/news.jsx
@@ -2,8 +2,8 @@ const News = () => {
     return <div className="text-justify">
         
         <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
-            <li>04/2025: We release <span className="font-bold"><a href='/openevents-v1' className="underline text-[1.1em] text-[#232ba1]">OpenEvents V1 dataset</a></span></li>
-            <li>02/2025: We will organize <span className="font-bold"><a href='/eventa-2025' className="underline text-[1.1em] text-[#232ba1]"> grand challenge EVENTA at ACM Multimedia 2025</a></span></li>
+            <li>04/2025: We release <span className="font-bold"><a href='./openevents-v1' className="underline text-[1.1em] text-[#232ba1]">OpenEvents V1 dataset</a></span></li>
+            <li>02/2025: We will organize <span className="font-bold"><a href='./eventa-2025' className="underline text-[1.1em] text-[#232ba1]"> grand challenge EVENTA at ACM Multimedia 2025</a></span></li>
         </ul>
     </div>
 }
--- src/app/eventa-2025/components/dataset.jsx
+++ src/app/eventa-2025/components/dataset.jsx
@@ -2,7 +2,7 @@ const Dataset = () => {
     return (
         <div className="text-justify">
            <p>
-           EVENTA 2025 Grand Challenge uses <span className="font-bold"><a href='./eventcap-v1' className="underline text-[1.1em] text-[#232ba1]">EventCap V1 dataset</a></span>.
+           EVENTA 2025 Grand Challenge uses <span className="font-bold"><a href='./openevents-v1' className="underline text-[1.1em] text-[#232ba1]">OpenEvents V1 dataset</a></span>.
            </p>
 
         </div>
