6b4019eb1fdf14820fa8b0a5e86dac64283631f5
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-04-14 00:52:40 +0700

    update

--- src/app/eventa-2025/components/organizers.jsx
+++ src/app/eventa-2025/components/organizers.jsx
@@ -7,11 +7,11 @@ const Organizers = ()=>{
             <PersonAvatar name="Trung-Nghia Le" image={`${imagePrefix}/ltnghia.jpg`} affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Minh-Triet Tran" image={`${imagePrefix}/tmtriet.png`} affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Tam Nguyen" image={`${imagePrefix}/nvtam.jpeg`} affiliation={"University of Dayton, US"} />
+            <PersonAvatar name="Thien-Phuc Tran" image={`${imagePrefix}/ttphuc.jpg`} affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Trong-Le Do" image={`${imagePrefix}/dtle.png`} affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Khanh-Duy Le" image={`${imagePrefix}/lkduy.jpg`} affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Duy-Nam Ly" image={`${imagePrefix}/ldnam.jpg`} affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Viet-Tham Huynh" image={`${imagePrefix}/hvtham.jpg`} affiliation={"University of Science, Vietnam"} />
-            <PersonAvatar name="Thien-Phuc Tran" image={`${imagePrefix}/ttphuc.jpg`} affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Minh-Quang Nguyen" image={`${imagePrefix}/nmquang.jpg`} affiliation={"University of Science, Vietnama"} />
             <PersonAvatar name="Phuc-Tan Nguyen" image={`${imagePrefix}/nptan.jpg`} affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Trong-Hieu Nguyen" image={`${imagePrefix}/nthieu.jpg`} affiliation={"University of Science, Vietnam"} />
