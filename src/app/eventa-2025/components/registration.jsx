afa225325e5b299e0d9272e3060c846e1ef1e28f
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-04-14 01:02:34 +0700

    update url

--- src/app/eventa-2025/components/registration.jsx
+++ src/app/eventa-2025/components/registration.jsx
@@ -2,7 +2,7 @@ const Registration = () => {
     return (
         <div className="text-justify">
            <p>
-           To participate in the EVENTA 2025 Grand Challenge, please first register by submitting <span className="font-bold"><a href='https://forms.gle/rtAeAPLp27TLLjHX8' className="underline text-[1.1em] text-[#232ba1]">the form</a></span>.
+           To participate in the EVENTA 2025 Grand Challenge, please first register by submitting <span className="font-bold"><a  target="_blank" href='https://forms.gle/rtAeAPLp27TLLjHX8' className="underline text-[1.1em] text-[#232ba1]">the form</a></span>.
            </p>
         </div>
     )
--- src/app/eventa-2025/track1/components/instructions.jsx
+++ src/app/eventa-2025/track1/components/instructions.jsx
@@ -2,11 +2,11 @@ const Instruction = () => {
     return (
         <div className="text-justify">
             <p>
-                Participants require to submit a CSV file with the format name (<span className="font-bold">TeamName_EVENTA2025_Track1.csv</span>) to <span className="font-bold"><a href='https://codalab.lisn.upsaclay.fr/competitions/' className="underline text-[1.1em] text-[#232ba1]">CodaLab</a></span>. The file has to be compressed to <span className="font-bold">submission.zip</span> before being submitted to CodaLab.
+                Participants require to submit a CSV file with the format name (<span className="font-bold">TeamName_EVENTA2025_Track1.csv</span>) to <span className="font-bold"><a  target="_blank" href='https://codalab.lisn.upsaclay.fr/competitions/' className="underline text-[1.1em] text-[#232ba1]">CodaLab</a></span>. The file has to be compressed to <span className="font-bold">submission.zip</span> before being submitted to CodaLab.
             </p>
             <br/>
             <p>
-                Participants also require to submit a detailed paper through <span className="font-bold"><a href='https://cmt3.research.microsoft.com/EVENTA2025' className="underline text-[1.1em] text-[#232ba1]">the official challenge platform</a></span> to validate their solutions.
+                Participants also require to submit a detailed paper through <span className="font-bold"><a  target="_blank" href='https://cmt3.research.microsoft.com/EVENTA2025' className="underline text-[1.1em] text-[#232ba1]">the official challenge platform</a></span> to validate their solutions.
             </p>
             <br/>
             <p>More details will be released soon.</p>
