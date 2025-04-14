7780b5e15138330a23d925dea085a45a2ef5a32b
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-04-14 21:19:48 +0700

    change date

--- src/app/eventa-2025/components/importantdates.jsx
+++ src/app/eventa-2025/components/importantdates.jsx
@@ -2,10 +2,11 @@ const ImportantDates = () => {
     return <div className="text-justify">
         
         <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
-            <li>Challenge opened: <span className="font-bold"> Apr. 15, 2025 </span></li>
+            <li>Challenge opened: <span className="font-bold"> Apr. 01, 2025 </span></li>
+            <li>Team registration opened: <span className="font-bold"> Apr. 01, 2025 </span></li>
             <li>Training set released: <span className="font-bold"> Apr. 15, 2025 </span></li>
             <li>Public-test set released: <span className="font-bold"> May. 15, 2025 </span></li>
-            <li>Private-test set released: <span className="font-bold"> May. 30, 2025 </span></li>
+            <li>Private-test set released: <span className="font-bold"> Jun. 10, 2025 </span></li>
             <li>Team registration deadline: <span className="font-bold"> Jun. 24, 2025 </span></li>
             <li>Challenge closed: <span className="font-bold"> Jun. 24, 2025 </span></li>
             <li>Paper submission deadline: <span className="font-bold"> Jul. 01, 2025 </span></li>
