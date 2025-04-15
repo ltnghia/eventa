2b62abb9e086f25c6aef3224616a55f867c968d1
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-04-15 11:32:07 +0700

    update rules

--- src/app/eventa-2025/components/rules.jsx
+++ src/app/eventa-2025/components/rules.jsx
@@ -4,7 +4,7 @@ const Rules = () => {
             <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
                 <li>Participants are not allowed to annotate the test sets.</li>
                 <li>External datasets are permitted. However, participants may only use publicly accessible datasets and pre-trained models. The use of private datasets or pre-trained models is strictly prohibited.</li>
-                <li>Commercial tools, libraries, APIs, etc. are strictly prohibited.</li>
+                <li>Open sources are allowed. However, commercial tools, libraries, APIs, etc. are strictly prohibited.</li>
                 <li>Final scores are calculated based on performance on the Private Test set.</li>
                 <li>Participants must make their source code publicly available on GitHub to ensure reproducibility.</li>
                 <li>Participants must submit a detailed paper through the official challenge platform before the deadline to validate their solutions.</li>
