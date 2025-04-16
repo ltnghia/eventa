10c0b3f04b0d6a77f33b7f8b0b9219d994925552
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-04-16 12:04:40 +0700

    update font size

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -1,16 +1,14 @@
-
-const Header = ()=>{
-    const highlight = "underline font-bold text-white"
+const Header = () => {
+    const highlight = "underline font-bold text-white";
     return (
         <div className="header">
-            <div className="justify-center flex flex-col sm:text-[2em] text-[1.5em] text-[#f5f5f5]  font-medium ">
-                <div className="text-white text-center text-[5em]">
-                   Event-Enriched Image Analysis (EVENTA)
+            <div className="justify-center flex flex-col sm:text-[6em] text-[4.5em] text-[#f5f5f5] font-medium">
+                <div className="text-white text-center text-[5.1em]">
+                    Event-Enriched Image Analysis (EVENTA)
                 </div>
             </div>
-           
-           
         </div>
     );
-}
+};
+
 export default Header;
