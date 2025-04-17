20dc7426d9de1b5485b0aaae0c51caa225784d4f
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-04-18 06:50:01 +0700

    update dates

--- src/app/components/news.jsx
+++ src/app/components/news.jsx
@@ -1,12 +1,44 @@
 import { imagePrefix } from "@/constant";
 
 const News = () => {
-    return <div className="text-justify">
-        
-        <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
-            <li>04/2025: We release <span className="font-bold"><a href={`${imagePrefix}/openevents-v1`} className="underline text-[1.1em] text-[#232ba1]">OpenEvents V1 dataset</a></span></li>
-            <li>02/2025: We will organize <span className="font-bold"><a href={`${imagePrefix}/eventa-2025`} className="underline text-[1.1em] text-[#232ba1]"> grand challenge EVENTA at ACM Multimedia 2025</a></span></li>
-        </ul>
+  return (
+    <div className="text-justify">
+      <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
+        <li>
+          <span className="font-bold">04/2025:</span>{" "}
+          <a
+            href={`${imagePrefix}/eventa-2025`}
+            className="underline text-[1.1em] text-[#232ba1]"
+          >
+            EVENTA 2025 Challenge
+          </a>{" "}
+          officially begins.
+        </li>
+        <li>
+          <span className="font-bold">04/2025:</span> We release the{" "}
+          <a
+            href={`${imagePrefix}/openevents-v1`}
+            className="underline text-[1.1em] text-[#232ba1]"
+          >
+            OpenEvents V1 dataset
+          </a>
+          .
+        </li>
+        <li>
+          <span className="font-bold">02/2025:</span> We will host the EVENTA Grand Challenge at{" "}
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://acmmm2025.org/grand-challenge"
+            className="underline text-[1.1em] text-[#232ba1]"
+          >
+            ACM Multimedia 2025
+          </a>
+          .
+        </li>
+      </ul>
     </div>
-}
+  );
+};
+
 export default News;
--- src/app/eventa-2025/components/importantdates.jsx
+++ src/app/eventa-2025/components/importantdates.jsx
@@ -4,9 +4,9 @@ const ImportantDates = () => {
         <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
             <li>Challenge opened: <span className="font-bold"> <span className="line-through">Apr. 01, 2025</span>  </span></li>
             <li>Team registration opened: <span className="font-bold"> <span className="line-through">Apr. 01, 2025</span> </span></li>
-            <li>Training set released: <span className="font-bold"> Apr. 17, 2025 </span></li>
-            <li>Public-test set released: <span className="font-bold"> May. 01, 2025 </span></li>
-            <li>Team registration deadline: <span className="font-bold"> Jun. 9, 2025 </span></li>
+            <li>Training set released: <span className="font-bold"> Apr. 18, 2025 </span></li>
+            <li>Public-test set released: <span className="font-bold"> May. 05, 2025 </span></li>
+            <li>Team registration deadline: <span className="font-bold"> Jun. 09, 2025 </span></li>
             <li>Private-test set released: <span className="font-bold"> Jun. 10, 2025 </span></li>
             <li>Challenge closed: <span className="font-bold"> Jun. 24, 2025 </span></li>
             <li>Paper submission deadline: <span className="font-bold"> Jul. 01, 2025 </span></li>
