642d27b03b8dd02f9551a4d96853949ea8bc5fcd
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-05-17 16:35:06 +0700

    update track 1

--- src/app/eventa-2025/track1/components/track1.jsx
+++ src/app/eventa-2025/track1/components/track1.jsx
@@ -6,7 +6,7 @@ const Track1 = () => {
            <p>
            This track aims to generate captions that provide richer, more comprehensive information about an image. 
            These captions go beyond simple visual descriptions by offering deeper insights, including the names and attributes of objects, the timing, context, outcomes of events, and other crucial details—information that cannot be gleaned from merely observing the image. 
-           <code className="text-blue-600 font-semibold">Given an image, participants are required to search relevant articles in a provided external database and extract necessary information to enrich the image caption.</code> 
+           <code className="text-blue-600 font-semibold">Given an image, participants are required to search relevant articles in a provided external database and extract necessary information to generate an enriched image caption.</code> 
            This retrieval augmentation generation track facilitates the creation of more coherent and detailed narratives, capturing not only the visible elements but also the underlying context and significance of the scene, ultimately offering a more complete understanding of what the image represents.
            </p>
            <div className="flex flex-col items-center">
