dce9a23228b2d92d0a5afc3e92f1bcd740b95f7c
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-07-24 22:40:20 +0700

    fix format

--- src/app/eventa-2025/components/acceptedpapers.jsx
+++ src/app/eventa-2025/components/acceptedpapers.jsx
@@ -1,14 +1,13 @@
 const AcceptedPapers = () => {
     return (
         <div className="text-justify">
-            <td className="border px-3 py-2 font-bold">ACM Multimedia 2025 Proceedings:</td>
+            <td className="px-3 py-2 font-bold">ACM Multimedia 2025 Proceedings:</td>
             <ol className="list-disc list-inside space-y-2 mt-2 mb-8 ml-4">
                 <li>Cerebro Team, &quot;ENRIC: EveNt-AwaRe Captioning with Image Retrieval via UnCertainty-Guided Re-ranking and Semantic Ensemble Reasoning&quot;. [<a href="https://drive.google.com/file/d/11h6cBv8Gi8cvXynqsflWT8aYl_Lmm_oB/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">PDF</a>] </li>
                 <li>SodaBread Team, &quot;ReCap: Event-Aware Image Captioning with Article Retrieval and Semantic Gaussian Normalization&quot;. [<a href="https://drive.google.com/file/d/112BpFQxf0boqeqjKvWA0UoPLluF6KCW1/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">PDF</a>] </li>
                 <li>NoResources Team, &quot;EVENT-Retriever: Event-Aware Multimodal Image Retrieval for Realistic Captions&quot;. [<a href="https://drive.google.com/file/d/1WY0WSVxVgkSkERsFiBbH20XZfciw2aJ9/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">PDF</a>] </li>
             </ol>
-            <br/>
-            <td className="border px-3 py-2 font-bold">Unachieved Papers:</td>
+            <td className="px-3 py-2 font-bold">Unachieved Papers:</td>
             <ol className="list-disc list-inside space-y-2 mt-2 mb-8 ml-4">
                 <li>Re:zero Slavery Team, &quot;Beyond Vision: Contextually Enriched Image Captioning with Multi-Modal Retrieval&quot;. [<a href="https://drive.google.com/file/d/1_-0QwLfkggtv_6t4AWOEB34_ba6S5AiL/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">PDF</a>] </li>
                 <li>ITxTK9 Team, &quot;ZSE-Cap: A Zero-Shot Ensemble for Image Retrieval and Prompt-Guided Captioning&quot;. [<a href="https://drive.google.com/file/d/1yo9ZlnXDU2B1qLcVF2IuMVPe9G8sxXqS/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">PDF</a>] </li>
