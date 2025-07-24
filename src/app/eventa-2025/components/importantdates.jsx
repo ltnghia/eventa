a55fa465147c26ee65a94eeef7a0fcb05206eb17
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-07-24 22:38:32 +0700

    update link

--- src/app/eventa-2025/components/importantdates.jsx
+++ src/app/eventa-2025/components/importantdates.jsx
@@ -7,10 +7,10 @@ const ImportantDates = () => {
             <li>Training set released: <span className="font-bold"> <span className="line-through">Apr. 18, 2025</span> </span></li>
             <li>Public-test set released: <span className="font-bold"> <span className="line-through">May. 05, 2025</span>  </span></li>
             <li>Team registration deadline: <span className="font-bold"> <span className="line-through">Jun. 09, 2025</span> </span></li>
-            <li>Private-test set released:  <del><span className="font-bold"> Jun. 10, 2025 </span></del></li>
-            <li>Challenge closed: <del><span className="font-bold"> Jun. 24, 2025 </span></del></li>
-            <li>Paper submission deadline: <span className="font-bold"> Jul. 01, 2025 </span></li>
-            <li>Acceptance notification: <span className="font-bold"> Jul. 24, 2025 </span></li>
+            <li>Private-test set released:  <del><span className="font-bold"> <span className="line-through">Jun. 10, 2025 </span></span></del></li>
+            <li>Challenge closed: <del><span className="font-bold"> <span className="line-through">Jun. 24, 2025 </span></span></del></li>
+            <li>Paper submission deadline: <span className="font-bold"> <span className="line-through">Jul. 01, 2025 </span></span></li>
+            <li>Acceptance notification: <span className="font-bold"> <span className="line-through">Jul. 24, 2025 </span></span></li>
             <li>Camera-ready deadline: <span className="font-bold"> Aug. 26, 2025 </span></li>
             <li>Challenge date: <span className="font-bold"> Otc. 27-31, 2025 </span></li>
         </ul>
