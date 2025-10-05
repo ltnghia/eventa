1f50c52c8926cd20abf14836afa1caab650556a2
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-10-05 17:20:24 +0700

    change info

--- src/app/eventa-2025/components/schedule.jsx
+++ src/app/eventa-2025/components/schedule.jsx
@@ -3,11 +3,11 @@ const Schedule = () => {
         The EVENTA Challenge will take place on <span className="font-bold"> October 31st, 2025 (Friday) from 9:00AM to 10:30AM (GMT+1)</span>. The event will be held at <strong>Hyatt, Distillers Room</strong> and online via Zoom. The detailed schedule is as follows:
         <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
             <li><span className="font-bold"> 9:00AM - 9:10AM </span> — Welcome and Introduction </li>
-            <li><span className="font-bold"> 9:10AM - 9:25AM </span> — <strong>Cerebro Team:</strong> ENRIC: EveNt-AwaRe Captioning with Image Retrieval via UnCertainty-Guided Re-ranking and Semantic Ensemble Reasoning</li>
-            <li><span className="font-bold"> 9:25AM - 9:40AM </span> — <strong>SodaBread Team:</strong> ReCap: Event-Aware Image Captioning with Article Retrieval and Semantic Gaussian Normalization</li>
-            <li><span className="font-bold"> 9:40AM - 9:55AM </span> — <strong>NoResources Team:</strong> EVENT-Retriever: Event-Aware Multimodal Image Retrieval for Realistic Captions</li>
-            <li><span className="font-bold"> 9:55AM - 10:10AM </span> — <strong>HCMUS-NoName Team:</strong> Hierarchical Multi-Modal Retrieval for Knowledge-Grounded News Image Captioning</li>
-            <li><span className="font-bold"> 10:10AM - 10:25AM </span> — <strong>23trinitrotoluen Team:</strong> A Hybrid Dense-Sparse Multi-Stage Re-ranking Framework for Event-Based Image Retrieval</li>
+            <li><span className="font-bold"> 9:10AM - 9:25AM </span> — <strong>Cerebro Team (onsite):</strong> ENRIC: EveNt-AwaRe Captioning with Image Retrieval via UnCertainty-Guided Re-ranking and Semantic Ensemble Reasoning</li>
+            <li><span className="font-bold"> 9:25AM - 9:40AM </span> — <strong>SodaBread Team (online):</strong> ReCap: Event-Aware Image Captioning with Article Retrieval and Semantic Gaussian Normalization</li>
+            <li><span className="font-bold"> 9:40AM - 9:55AM </span> — <strong>NoResources Team (online):</strong> EVENT-Retriever: Event-Aware Multimodal Image Retrieval for Realistic Captions</li>
+            <li><span className="font-bold"> 9:55AM - 10:10AM </span> — <strong>HCMUS-NoName Team (online):</strong> Hierarchical Multi-Modal Retrieval for Knowledge-Grounded News Image Captioning</li>
+            <li><span className="font-bold"> 10:10AM - 10:25AM </span> — <strong>23trinitrotoluen Team (online):</strong> A Hybrid Dense-Sparse Multi-Stage Re-ranking Framework for Event-Based Image Retrieval</li>
             <li><span className="font-bold"> 10:25AM </span> — Closing Remark</li>
         </ul>
         <p className="mt-4 italic text-gray-600">
