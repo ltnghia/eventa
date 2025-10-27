6c59a124e3ea563a799f479e5023c6d751498e6f
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-10-28 06:27:24 +0700

    update papers

--- src/app/eventa-2025/components/acceptedpapers.jsx
+++ src/app/eventa-2025/components/acceptedpapers.jsx
@@ -3,9 +3,9 @@ const AcceptedPapers = () => {
         <div className="text-justify">
             <td className="px-3 py-2 font-bold">ACM Multimedia 2025 Proceedings:</td>
             <ol className="list-disc list-inside space-y-2 mt-2 mb-8 ml-4">
-                <li>Cerebro Team, &quot;ENRIC: EveNt-AwaRe Captioning with Image Retrieval via UnCertainty-Guided Re-ranking and Semantic Ensemble Reasoning&quot;. [<a href="https://drive.google.com/file/d/11h6cBv8Gi8cvXynqsflWT8aYl_Lmm_oB/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">PDF</a>] </li>
-                <li>SodaBread Team, &quot;ReCap: Event-Aware Image Captioning with Article Retrieval and Semantic Gaussian Normalization&quot;. [<a href="https://drive.google.com/file/d/112BpFQxf0boqeqjKvWA0UoPLluF6KCW1/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">PDF</a>] </li>
-                <li>NoResources Team, &quot;EVENT-Retriever: Event-Aware Multimodal Image Retrieval for Realistic Captions&quot;. [<a href="https://drive.google.com/file/d/1WY0WSVxVgkSkERsFiBbH20XZfciw2aJ9/view?usp=drive_link" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">PDF</a>] </li>
+                <li>Cerebro Team, &quot;ENRIC: EveNt-AwaRe Captioning with Image Retrieval via UnCertainty-Guided Re-ranking and Semantic Ensemble Reasoning&quot;. [<a href="https://dl.acm.org/doi/10.1145/3746027.3762090" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">PDF</a>] </li>
+                <li>SodaBread Team, &quot;ReCap: Event-Aware Image Captioning with Article Retrieval and Semantic Gaussian Normalization&quot;. [<a href="https://dl.acm.org/doi/10.1145/3746027.3762039" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">PDF</a>] </li>
+                <li>NoResources Team, &quot;EVENT-Retriever: Event-Aware Multimodal Image Retrieval for Realistic Captions&quot;. [<a href="https://dl.acm.org/doi/10.1145/3746027.3762038" target="_blank" rel="noopener noreferrer" className="text-blue-600 hover:underline">PDF</a>] </li>
             </ol>
             <td className="px-3 py-2 font-bold">Unachieved Papers:</td>
             <ol className="list-disc list-inside space-y-2 mt-2 mb-8 ml-4">
