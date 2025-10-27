6817f3514f37fd74605d8b81a72376e8694002ac
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-10-27 23:23:04 +0700

    update schedule

--- src/app/eventa-2025/components/schedule.jsx
+++ src/app/eventa-2025/components/schedule.jsx
@@ -17,13 +17,12 @@ const Schedule = () => {
       </p>
 
       <ul className="list-disc ml-6 space-y-1">
-        <li><strong>09:00 – 09:10</strong> Welcome and Introduction</li>
-        <li><strong>09:10 – 09:25</strong> <strong>Cerebro Team (onsite):</strong> ENRIC: EveNt-AwaRe Captioning with Image Retrieval via UnCertainty-Guided Re-ranking and Semantic Ensemble Reasoning</li>
-        <li><strong>09:25 – 09:40</strong> <strong>SodaBread Team (online):</strong> ReCap: Event-Aware Image Captioning with Article Retrieval and Semantic Gaussian Normalization</li>
-        <li><strong>09:40 – 09:55</strong> <strong>NoResources Team (online):</strong> EVENT-Retriever: Event-Aware Multimodal Image Retrieval for Realistic Captions</li>
-        <li><strong>09:55 – 10:10</strong> <strong>HCMUS-NoName Team (online):</strong> Hierarchical Multi-Modal Retrieval for Knowledge-Grounded News Image Captioning</li>
-        <li><strong>10:10 – 10:25</strong> <strong>23trinitrotoluen Team (online):</strong> A Hybrid Dense-Sparse Multi-Stage Re-ranking Framework for Event-Based Image Retrieval</li>
-        <li><strong>10:25</strong> Closing Remark</li>
+        <li><strong>09:00 – 09:10</strong> Opening Remarks</li>
+        <li><strong>09:10 – 09:30</strong> <strong>Cerebro Team (onsite):</strong> ENRIC: EveNt-AwaRe Captioning with Image Retrieval via UnCertainty-Guided Re-ranking and Semantic Ensemble Reasoning</li>
+        <li><strong>09:30 – 09:50</strong> <strong>SodaBread Team (online):</strong> ReCap: Event-Aware Image Captioning with Article Retrieval and Semantic Gaussian Normalization</li>
+        <li><strong>09:50 – 09:10</strong> <strong>NoResources Team (online):</strong> EVENT-Retriever: Event-Aware Multimodal Image Retrieval for Realistic Captions</li>
+        <li><strong>09:10 – 10:30</strong> <strong>HCMUS-NoName Team (online):</strong> Hierarchical Multi-Modal Retrieval for Knowledge-Grounded News Image Captioning</li>
+        <li><strong>10:30</strong> Closing Remarks</li>
       </ul>
     </div>
   );
