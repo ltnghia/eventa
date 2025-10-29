4dbea02d2eb9cc8669880c3feccc928e434f422d
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-10-29 18:56:02 +0700

    update schedule

--- src/app/eventa-2025/components/schedule.jsx
+++ src/app/eventa-2025/components/schedule.jsx
@@ -17,13 +17,13 @@ const Schedule = () => {
       </p>
 
       <ul className="list-disc ml-6 space-y-1">
-        <li><strong>09:00 – 09:10</strong> - Opening Remarks</li>
-        <li><strong>09:10 – 09:25</strong> - <strong>Thien-Phuc Tran, et al. (Online):</strong> Event-Enriched Image Analysis Grand Challenge at ACM Multimedia 2025</li>
+        <li><strong>09:00 – 09:10</strong> - <strong>Prof. Minh-Triet Tran:</strong> Opening Remarks</li>
+        <li><strong>09:10 – 09:25</strong> - <strong>Thien-Phuc Tran, et al. (online):</strong> Event-Enriched Image Analysis Grand Challenge at ACM Multimedia 2025</li>
         <li><strong>09:25 – 09:40</strong> - <strong>Nam-Quan Nguyen, et al. (Cerebro Team - onsite):</strong> ENRIC: EveNt-AwaRe Captioning with Image Retrieval via UnCertainty-Guided Re-ranking and Semantic Ensemble Reasoning</li>
         <li><strong>09:40 – 09:55</strong> - <strong>Thinh-Phuc Nguyen, et al. (SodaBread Team - online):</strong> ReCap: Event-Aware Image Captioning with Article Retrieval and Semantic Gaussian Normalization</li>
         <li><strong>09:55 – 09:10</strong> - <strong>Dinh-Khoi Vo, et al. (NoResources Team - online):</strong> EVENT-Retriever: Event-Aware Multimodal Image Retrieval for Realistic Captions</li>
         <li><strong>09:10 – 10:25</strong> - <strong>Minh-Loi Nguyen, et al. (HCMUS-NoName Team - online):</strong> Hierarchical Multi-Modal Retrieval for Knowledge-Grounded News Image Captioning</li>
-        <li><strong>10:25</strong> - Closing Remarks</li>
+        <li><strong>10:25</strong> - <strong>Prof. Minh-Triet Tran:</strong> Closing Remarks</li>
       </ul>
     </div>
   );
